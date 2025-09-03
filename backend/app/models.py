63a71befd51dfdeb8efeb89c73da4766f5300007
pradeepika28 <pradeepikamakkena.123@gmail.com>
2025-09-03 23:05:07 +0530

    all changes done

--- backend/app/models.py
+++ backend/app/models.py
@@ -12,7 +12,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    is_admin = Column(Boolean, default=False) # <-- ADD THIS LINE
+    is_admin = Column(Boolean, default=False) 
 
     receipts = relationship("Receipt", back_populates="owner")
 
